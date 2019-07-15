2e73adf07556c3be37bf595c1baf2fca8ec7f289
Edwing123 <egarciavale2014@gmail.com>
2019-07-15 02:19:18 -0600

    Avanze de la cabecera de la pagina

--- portafolio-website/src/components/layout/page-header.vue
+++ portafolio-website/src/components/layout/page-header.vue
@@ -1,23 +1,24 @@
 <template>
   <header class="header row">
-    <div class="col-2">
+    <div class="col-auto">
       <div class="brand">
         <div class="brand__logo">
           <a href="/" title="Wink">
-            <img src="@/assets/images/logo.svg" alt="Wink logo" title="Wink logo" class="logo-image" />
+            <img src="@/assets/images/logo.svg" alt="Wink logo" title="Wink logo" class="logo__image" />
           </a>
         </div>
       </div>
     </div>
-    <nav class="nav col-6">
+    <nav class="nav col-auto">
       <router-link to="/"><a class="nav__item">Sobre nosotros</a></router-link>
       <router-link to="/"><a class="nav__item">Servicios</a></router-link>
       <router-link to="/"><a class="nav__item">Cotizar</a></router-link>
       <router-link to="/"><a class="nav__item">Contacto</a></router-link>
     </nav>
-    <div class="col-2">
-      <div class="language-switcher">
-        <span class="icon-language">Language</span>
+    <div class="col-auto">
+      <div class="lang-switcher">
+        <img src="@/assets/images/spanish-lang.svg" alt="Spain flag" class="lang" title="spanish" />
+        <img src="@/assets/images/english-lang.svg" alt="USA flag" class="lang" title="english" />
       </div>
     </div>
   </header>
