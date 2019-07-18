a8276dc15b20a5fa6a66478dfd0a2c90d313f782
Joseph Zamora <joseph.zamora64@yahoo.com>
2019-07-18 15:56:54 -0600

    Sample serializer

--- Portfolio/Email/views.py
+++ Portfolio/Email/views.py
@@ -3,6 +3,19 @@ from rest_framework.decorators import api_view
 from django.core.mail import EmailMessage
 from django.http import JsonResponse
 
+#class SendEmailSerializer(Serializer.Serializer):
+    #Email = serializers.Charfield()
+
+#class SendEmailView(viewset.ModelViewset):
+    
+    #def post(request):
+        #serialized_data = SendEmail(request.POST)
+        #if serialized_data.is_valid():
+            #return Response({"detail":"enviado"})
+        #return Response(serialized_data.errors)
+# Ejemplo de viewset para hacer CRUD
+#class SendEmailView(viewset.ModelViewset):
+    #model = Correos
 
 @api_view(['POST'])
 def SendEmail(request):
