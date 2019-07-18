88f2c1a017432762889ddff23a10a5458f9c1c80
Joseph Zamora <joseph.zamora64@yahoo.com>
2019-07-17 20:09:39 -0600

    Axios test

--- portafolio-website/src/views/Home.vue
+++ portafolio-website/src/views/Home.vue
@@ -15,3 +15,36 @@
   </div>
   </div>
 </template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://localhost:8000/`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
