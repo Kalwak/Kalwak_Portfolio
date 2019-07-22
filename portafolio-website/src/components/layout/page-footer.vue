819131ce99245c6cbe95978cacb30cb8d884edae
Edwing123 <egarciavale2014@gmail.com>
2019-07-21 20:00:32 -0600

    - contact form markup finished

--- portafolio-website/src/components/layout/page-footer.vue
+++ portafolio-website/src/components/layout/page-footer.vue
@@ -1,37 +1,48 @@
 <template>
   <footer class="footer row">
     <div class="col-12 inner-wrapper">
-      <div class="row">
-        <div class="col-6">
-          <div class="contact-form">
-            <h4>Contactanos</h4>
-            <div class="row">
-              <div class="col-12">
-                <div class="form-group">
-                  <input type="text" class="form-control" placeholder="NOMBRE" />
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="form-group">
-                  <input type="tel" class="form-control" placeholder="TELEFONO" />
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-MAIL" />
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-group">
-                  <textarea class="form-control" placeholder="Consulta"></textarea>
-                </div>
-              </div>
+      <div class="contact-section row">
+        <div class="contact-form col-8">
+          <div class="row">
+            <div class="col-12">
+              <h4 class="contact__title">Contactanos</h4>
+            </div>
+            <div class="form-group col-12">
+              <input type="text" class="form-control" placeholder="Nombre" />
+            </div>
+            <div class="form-group col">
+              <input type="tel" class="form-control" placeholder="Telefono" />
+            </div>
+            <div class="form-group col">
+              <input type="email" class="form-control" placeholder="E-mail" />
+            </div>
+            <div class="form-group col-12">
+              <textarea class="form-control" placeholder="Consulta"></textarea>
+            </div>
+            <div class="form-group col-12 text-right">
+              <button class="btn btn-success">Enviar</button>
             </div>
           </div>
         </div>
-        <div class="col-5">
-          <div class="contact-information">
-            <h4>Mas informacion al:</h4>
+
+        <div class="contact-information co-4">
+          <h4 class="contact__title">Mas information</h4>
+          <p class="contact-info">
+            <span class="info__title">Telefono:</span>
+            <span class="info__description">
+              <a href="tel:+506">+50684599023</a>
+            </span>
+          </p>
+          <p class="contact-info">
+            <span class="info__title">Correo:</span>
+            <span class="info__description">
+              <a href="mailto:kalwakcr@gmail.com">kalwakcr@gmail.com</a>
+            </span>
+          </p>
+          <h4 class="contact__title">Nuestras redes</h4>
+          <div class="social-media">
+            <a href="" class="social-media__icon icon-menu"></a>
+            <a href="" class="social-media__icon icon-menu"></a>
           </div>
         </div>
       </div>
