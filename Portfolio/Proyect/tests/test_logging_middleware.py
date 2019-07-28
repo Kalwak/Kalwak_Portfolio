b3df0497bb276dc480393e74d5d9700170bc8120
Joseph Zamora <joseph.zamora64@yahoo.com>
2019-07-27 20:12:59 -0600

    Tests for middleware logging

---
+++ Portfolio/Proyect/tests/test_logging_middleware.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from Proyect.models import Logs
+
+
+class LogsTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_log_creation(self):
+        self.client.get("/api/send_email")
+        expected = 1
+        log = Logs.objects.all().first()
+        log_count = Logs.objects.all().count()
+        self.assertEqual(expected, log_count)
+        log_status_code = log.status_code
+        expected = status.HTTP_405_METHOD_NOT_ALLOWED
+        self.assertEqual(expected, log_status_code)
+
+    def test_log_404(self):
+        self.client.get("/fakeurl/")
+        expected = status.HTTP_404_NOT_FOUND
+        status_code = Logs.objects.all().last().status_code
+        self.assertEqual(expected, status_code)
+
+    def test_log_200(self):
+        data = {
+            "ok": 1
+        }
+        self.client.post("/api/send_email", data=data, content_type="application/json")
+        status_code = Logs.objects.last().status_code
+        expected = status.HTTP_400_BAD_REQUEST
+        self.assertEqual(expected, status_code)
+
