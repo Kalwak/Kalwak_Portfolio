c63fb93ea5588cfa36df93595725f315d3cbcb4e
Edwing123 <egarciavale2014@gmail.com>
2019-08-11 20:05:20 -0600

    Modified link preview (open graph object)

--- portafolio-website/vue.config.js
+++ portafolio-website/vue.config.js
@@ -1,12 +1,4 @@
 const path = require('path');
 
 module.exports = {
-  // chainWebpack: config => {
-  //   if (process.env.NODE_ENV === 'production') {
-  //     config.module.rule('vue').uses.delete('cache-loader');
-  //     config.module.rule('js').uses.delete('cache-loader');
-  //     config.module.rule('ts').uses.delete('cache-loader');
-  //     config.module.rule('tsx').uses.delete('cache-loader');
-  //   }
-  // }
-}
+};
