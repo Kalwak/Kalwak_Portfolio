377a5b216b94df9d2434464a40673fd3c5319ad2
Joseph David Zamora Murillo <joseph@localhost.localdomain>
2019-08-16 04:57:01 +0000

    Deployment prep

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -169,6 +169,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/assets/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = "Kalwak Administration"
 handler404 = 'frontend.views.vuejs_frontend404'
